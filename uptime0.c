4d275b8e119e4f90ffef8f19662f8a66b238ab3d
Rockatansky04 <barrysocial520@gmail.com>
2025-06-11 10:37:25 -0600

    Update uptime0.c

--- uptime0.c
+++ uptime0.c
@@ -1,11 +1,3 @@
-static int uptime_show(struct seq_file *m, void *v) {
-    unsigned long current_time = jiffies;  
-    unsigned long uptime = (current_time - start_time) / HZ;  
-
-    seq_printf(m, "Uptime del módulo: %lu segundos\n", uptime);
-    return 0; 
-}
-
 static int uptime_open(struct inode *inode, struct file *file) {
     return single_open(file, uptime_show, NULL);
 }
