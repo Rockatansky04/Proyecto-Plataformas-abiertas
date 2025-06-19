2ef8e2498e24c5aca93bfc667415218f5e3de999
danibrec <brenescdaniel@gmail.com>
2025-06-18 22:48:12 -0600

    Actualizar uptime_mod.c
    Se agregó lo que fue a su vez subido en el commit de "Actualizar uptime_show" para que se pudiera implementar correctamente en el módulo.

--- uptime_mod.c
+++ uptime_mod.c
@@ -11,10 +11,14 @@ static unsigned long start_time;
 
 static int uptime_show(struct seq_file *m, void *v) {
     unsigned long current_time = jiffies;  
-    unsigned long uptime = (current_time - start_time) / HZ;  
+    unsigned long uptime = (current_time - start_time);  
+    unsigned long uptime_seconds = uptime_jiffies / HZ;
 
-    seq_printf(m, "Uptime del módulo: %lu segundos\n", uptime);  //Acá ya está contemplada una de las funciones que nos da Kerngod
-
+    seq_printf(m, "Tiempo de carga (jiffies): %lu\n", start_time); // Tiempo de carga del módulo
+    seq_printf(m, "Tiempo actual (jiffies): %lu\n", current_time); // Tiempo actual del sistema
+    seq_printf(m, "Uptime (jiffies): %lu\n", uptime_jiffies); // Uptime del módulo en jiffies
+    seq_printf(m, "Uptime (segundos): %lu\n", uptime_seconds); // Uptime del módulo en segundos
+    
     return 0; 
 }
 
