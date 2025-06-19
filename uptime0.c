88b518de6874a02497ee831ee31727ef75d3ae1a
danibrec <brenescdaniel@gmail.com>
2025-06-19 09:34:22 -0600

    Actualizar uptime_open.c
    Detalles menores: un comentario para complementar el código.

--- uptime0.c
+++ uptime0.c
@@ -1,3 +1,3 @@
 static int uptime_open(struct inode *inode, struct file *file) {
-    return single_open(file, uptime_show, NULL);
+    return single_open(file, uptime_show, NULL); // Esto activa el uptime_show.c
 }
