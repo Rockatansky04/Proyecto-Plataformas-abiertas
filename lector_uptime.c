fd2aef35426450c722c292580b22e87ec93e3aab
Luis-Martinez-H044 <luis.martinezherrera@ucr.ac.cr>
2025-06-19 09:35:23 -0600

    Update lector_uptime.c
    Se inició con el C

--- lector_uptime.c
+++ lector_uptime.c
@@ -1,1 +1,12 @@
 // Se identificó que se necesitaría un programa en C para que exista una interacción con el usuario en el espacio del mismo, el cual lleva por nombre lector_uptime.c
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+char respuesta;
+printf("Desea cargar el modulo uptime? (S/N)");
+scanf("%c", &respuesta);
+
+if (respuesta == 'S' || respuesta == 's'){
+        printf("Se procederá a cargar el modulo");
+        system("sudo insmod uptime_mod.ko");
