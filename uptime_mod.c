bc659745197f6678e1054e253d225d158550360f
Rockatansky04 <barrysocial520@gmail.com>
2025-07-02 23:17:37 -0600

    Update uptime_mod.c
    Errores por variables sin mencionar corregidos

--- uptime_mod.c
+++ uptime_mod.c
@@ -1,9 +1,9 @@
-#include <linux/module.h>   //Convierte el programa en modulo
-#include <linux/kernel.h>   //Nos permite usar el pr_info()
-#include <linux/init.h>     //Permite el uso de las funcion de inicio     
-#include <linux/proc_fs.h>  //Nos permite crear los archivos virtuales con proc()
-#include <linux/seq_file.h> //Permite escribir conenido en proc     
-#include <linux/jiffies.h>  //Nos permite acceder al tiempo desde el boot del sistema
+#include <linux/module.h>   // Convierte el programa en modulo
+#include <linux/kernel.h>   // Nos permite usar el pr_info()
+#include <linux/init.h>     // Permite el uso de las funcion de inicio     
+#include <linux/proc_fs.h>  // Nos permite crear los archivos virtuales con proc()
+#include <linux/seq_file.h> // Permite escribir contenido en proc     
+#include <linux/jiffies.h>  // Nos permite acceder al tiempo desde el boot del sistema
 
 #define PROC_NAME "uptime_mod"
 
@@ -11,7 +11,7 @@ static unsigned long start_time;
 
 static int uptime_show(struct seq_file *m, void *v) {
     unsigned long current_time = jiffies;  
-    unsigned long uptime = (current_time - start_time);  
+    unsigned long uptime_jiffies = (current_time - start_time);  
     unsigned long uptime_seconds = uptime_jiffies / HZ;
 
     seq_printf(m, "Tiempo de carga (jiffies): %lu\n", start_time); // Tiempo de carga del módulo
@@ -27,10 +27,10 @@ static int uptime_open(struct inode *inode, struct file *file) {
 }
 
 static const struct proc_ops uptime_fops = {
-    .proc_open    = uptime_open, //Desde acá se abre el archivo
-    .proc_read    = seq_read,  // Con esta función se lee
-    .proc_lseek   = seq_lseek, // Esta permite moverse en el archivo
-    .proc_release = single_release //Libera el archivo
+    .proc_open    = uptime_open, // Desde acá se abre el archivo
+    .proc_read    = seq_read,    // Con esta función se lee
+    .proc_lseek   = seq_lseek,   // Esta permite moverse en el archivo
+    .proc_release = single_release // Libera el archivo
 };
 
 static int __init uptime_mod_init(void) {
@@ -49,7 +49,6 @@ static void __exit uptime_mod_exit(void) {
     remove_proc_entry(PROC_NAME, NULL);
     pr_info("uptime_mod removido.\n");
 }
-
 
 module_init(uptime_mod_init);
 module_exit(uptime_mod_exit);
